8b38fa68948f3db20f0eb2a712f33a36e0440ba6
karima2040 <karima2040@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-04-27 12:15:22 +0000

    내일 화이팅!

--- trunk/PhoneGapMyPlugin/assets/www/myPlugin.js
+++ trunk/PhoneGapMyPlugin/assets/www/myPlugin.js
@@ -6,7 +6,9 @@ var myPlugin = function(){
 	this.sendData = function(onSuccess,onError,data){
 		PhoneGap.exec(onSuccess,onError,"com.phonegap.plugin.myPlugin","sendData",[data]);
 	};
-	
+	this.TvSerch = function(onSuccess,onError,data){
+		PhoneGap.exec(onSuccess,onError,"com.phonegap.plugin.myPlugin","TvSerch",[data]);
+	};
 };
 PhoneGap.addConstructor(function(){
 	PhoneGap.addPlugin('myplugin',new myPlugin());
