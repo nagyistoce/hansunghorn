8a1b3fe312edd4b3dc7c910561a177279db7b691
karima2040 <karima2040@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-04-29 13:19:58 +0000

    4-29 day

--- trunk/PhoneGapMyPlugin/src/com/phonegapmyplugin/mypluginActivity.java
+++ trunk/PhoneGapMyPlugin/src/com/phonegapmyplugin/mypluginActivity.java
@@ -1,13 +1,52 @@
 package com.phonegapmyplugin;
 
 import com.phonegap.*;
+import com.phonegapmyplugin.R.id;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import SOD.Common.*;
+import SOD.Deprecated.*;
+import SOD.Deprecated.Phone.*;
+import SOD.Deprecated.TV.*;
+import SOD.SmartPhone.*;
+import SOD.SmartTV.*;
 
 public class mypluginActivity extends DroidGap {
-    /** Called when the activity is first created. */
-    @Override
+	Button TvSearch,ServiceLocation,InstallService;
+	public void Initialize()
+	{
+		  super.loadUrl("file:///android_asset/www/index.html");
+	}
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        setContentView(R.layout.main);
+        
+        
+        TvSearch=(Button)findViewById(R.id.TvSearch);
+        ServiceLocation=(Button)findViewById(R.id.ServiceLocation);
+        InstallService=(Button)findViewById(R.id.InstallService);
+        
+        TvSearch.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				//new AccessManager().searchServer();	 // TV 서버 찾기
+			}
+		});
+        ServiceLocation.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				
+			}
+		});
+        InstallService.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Initialize();
+			}
+		});
+      
     }
 }
