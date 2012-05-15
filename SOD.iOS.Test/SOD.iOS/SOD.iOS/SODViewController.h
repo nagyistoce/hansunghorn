86830cf207b3217b2058957eacba3df6393bd51b
maskkwon@gmail.com <maskkwon@gmail.com@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-05-15 11:30:31 +0000

    iOS Send 테스트 코드

---
+++ SOD.iOS.Test/SOD.iOS/SOD.iOS/SODViewController.h
@@ -0,0 +1,14 @@
+//
+//  SODViewController.h
+//  SOD.iOS
+//
+//  Created by Project BJ on 12. 4. 25..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SODViewController : UIViewController
+{
+}
+@end
