f9bea02a8634c54a9f336e0ccddab43c812ae186
maskkwon@gmail.com <maskkwon@gmail.com@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-05-15 11:29:22 +0000

    iOS 직렬화 테스트 코드

---
+++ SOD.iOS.Test/TestXML/TestXMLTests/TestXMLTests.m
@@ -0,0 +1,32 @@
+//
+//  TestXMLTests.m
+//  TestXMLTests
+//
+//  Created by Project BJ on 12. 5. 12..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestXMLTests.h"
+
+@implementation TestXMLTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestXMLTests");
+}
+
+@end
