e74ee745171884331863e98acbc5939e19d278dc
maskkwon@gmail.com <maskkwon@gmail.com@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-05-15 11:29:22 +0000

    iOS 직렬화 테스트 코드

---
+++ trunk/SOD.iOS.Test/TestXML/TestXML/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestXML
+//
+//  Created by Project BJ on 12. 5. 12..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ trunk/SOD.iOS.Test/TestXML/TestXMLTests/TestXMLTests.h
@@ -0,0 +1,13 @@
+//
+//  TestXMLTests.h
+//  TestXMLTests
+//
+//  Created by Project BJ on 12. 5. 12..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestXMLTests : SenTestCase
+
+@end
