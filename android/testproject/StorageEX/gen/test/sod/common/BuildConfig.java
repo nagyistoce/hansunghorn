407cc15b9a2f1d49c473b6bfc75c7faaa6748830
maskkwon@gmail.com <maskkwon@gmail.com@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-09-10 06:39:51 +0000

    move test projects

--- android/testproject/StorageEX/gen/test/sod/common/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package test.sod.common;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- android/testproject/TVLocationManagerEX2/gen/sod/test/smarttv/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package sod.test.smarttv;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
