f16e9f4296676a2c1c89fc6cf6a4934199b755ee
karima2040 <karima2040@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-09-19 10:56:01 +0000

    9.19일 패 왔다리 갔다리

---
+++ trunk/PHoneService/src/phone/bean/GCCBean.java
@@ -0,0 +1,19 @@
+package phone.bean;
+
+import java.io.IOException;
+
+import phone.beanImpl.ServiceBeanImpl;
+
+public class GCCBean implements ServiceBeanImpl{
+
+	public void ReceiveData() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void SendData(String data) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
