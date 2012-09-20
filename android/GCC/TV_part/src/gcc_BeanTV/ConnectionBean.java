89c8cd93d2fd054f1e3ad7a5b65adbdd2b684b3a
djawodud87 <djawodud87@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-09-20 04:52:51 +0000

    프레임워크 UI부분 추가

---
+++ android/GCC/TV_part/src/gcc_BeanTV/ConnectionBean.java
@@ -0,0 +1,15 @@
+package gcc_BeanTV;
+
+import sod.smarttv.AccessManagerServer;
+import sod.smarttv.ServerConfig;
+
+public class ConnectionBean
+{
+	public static final int SERVERPORT = 30331;
+	public static final String SERVERIP = "127.0.0.1";
+	public static String Message;
+	public static AccessManagerServer server;
+	public static ConnectionBean con;
+	public static ServerConfig ServerConfig;
+	public static int ClientId;
+}
