bfa600485c42bad6f0c9bfb84f75df06d81fc910
djawodud87 <djawodud87@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-09-21 07:37:50 +0000

    파일 다운로드 기능 추가.. 버그 수정 필요

--- android/GCC/TV_part/src/sod/common/Constants.java
+++ android/GCC/TV_part/src/sod/common/Constants.java
@@ -9,11 +9,11 @@ public class Constants {
 	public static final int Default_Timeout = 30000;
 	
 	public static final String Multicast_IP = "224.0.0.1";
-	public static final int Multicast_Port = 4020;
-	public static final int Multicast_Port_Response = 4021;
+	public static final int Multicast_Port_Send = 37401;
+	public static final int Multicast_Port_Response = 37402;
 
 	public static final Logable logger = new Logable() {			
-
+		@Override
 		public void log(Object arg) {
 			Log.d("logger", arg.toString());
 			
