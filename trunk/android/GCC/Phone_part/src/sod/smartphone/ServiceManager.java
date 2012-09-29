55ea39bf70b7a77e4c383aa2ef425c0e1cdef742
djawodud87 <djawodud87@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-09-29 06:50:27 +0000

    sod.smartphone
    ServiceManager.java
    isExistService
    checkServiceInstalled로 변경

--- trunk/android/GCC/Phone_part/src/sod/smartphone/ServiceManager.java
+++ trunk/android/GCC/Phone_part/src/sod/smartphone/ServiceManager.java
@@ -263,7 +263,7 @@ public class ServiceManager {
 
 	
 	
-	public boolean isExistService(String serviceName){
+	public boolean checkServiceInstalled(String serviceName){
 		
 		return Storage.checkStorageIs(serviceName);
 	}
