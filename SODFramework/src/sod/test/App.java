63c4f8c63024976c26ce6a786e59361ea41efbe8
djawodud87@gmail.com <djawodud87@gmail.com@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-09-30 07:48:59 +0000

    테스트코드 추가

--- SODFramework/src/sod/test/App.java
+++ SODFramework/src/sod/test/App.java
@@ -1,7 +1,16 @@
 package sod.test;
 
+import java.net.InetSocketAddress;
+
 import sod.common.ConsoleLogger;
 import sod.common.Constants;
+import sod.common.Packet;
+import sod.common.Transceiver;
+import sod.smarttv.AccessManagerServer;
+import sod.smarttv.ConnectHandler;
+import sod.smarttv.DisconnectHandler;
+import sod.smarttv.ServerConfig;
+import sod.smarttv.ServerReceiveHandler;
 
 public class App {
 
@@ -9,12 +18,58 @@ public class App {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//Test.initServer();
 		//Test.testSerializer();
 		//Test.testTransceiver();	
-		Test.testAccessManager();
-		//Test.testServerSearch();
+		//Test.testAccessManager();
+		Test.testServerSearch();
 		//Test.testServer();
 		//Test.testClient();
 	}
+	/*
+	public static void initServer() {
+		// server setup
+		server = new AccessManagerServer();
+		ServerConfig conf = new ServerConfig();
+		conf.Port = ServerPort;
+		conf.serviceName = "TestService";
+
+		server.setConnectHandler(new ConnectHandler() {
+			@Override
+			public void onConnect(int connid) {
+				logger.log("(server): new connection is accepted - " + connid
+						+ "\n");
+			}
+		});
+		server.setDisconnectHandler(new DisconnectHandler() {
+
+			@Override
+			public void onDisconnect(int connid) {
+				logger.log("(server): client is disconnected - " + connid
+						+ "\n");
+			}
+		});
+		server.setReceiveHandler(new ServerReceiveHandler() {
+			
+			Transceiver trans= new Transceiver(new InetSocketAddress("127.168.0.26", 2013));
+			
+			@Override
+			public void onReceive(Packet pkt, int connid) {
+				logger.log("(server): a packet from client - " + connid + "\n");
+				server.send(pkt, connid);
+				
+				trans.send(pkt);	
+				
+			}
+		});
+
+		server.start(conf);
+		
+		logger.log("press enter to finish.\n");
+		waitfor();
+		
+		server.shutdown();
+	}
+	*/
 
 }
--- SODFramework/src/sod/test/Test.java
+++ SODFramework/src/sod/test/Test.java
@@ -21,84 +21,81 @@ import sod.common.*;
 
 public class Test {
 	final static String ServerIP = "127.0.0.1";
-	final static int ServerPort = 6721; 
+	final static int ServerPort = 6721;
 	final static int CheckingPeriod = Constants.Default_CheckingPeriod;
-	
+
 	static Logable logger;
-	static AccessManagerServer server; 
+	static AccessManagerServer server;
 	static AccessManager client;
-	static{
+	static {
 		logger = ConsoleLogger.getInstance();
 	}
-	
-	public static void waitfor(){
+
+	public static void waitfor() {
 		try {
 			System.in.read();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void testServerSearch(){		
-		//server setup
+
+	public static void testServerSearch() {
+		// server setup
 		server = new AccessManagerServer();
 		ServerConfig conf = new ServerConfig();
 		conf.Port = ServerPort;
 		conf.serviceName = "TestService";
-		server.start(conf);		
-		
-		//now we are looking for server.
-//		String localip = NetworkUtils.getLocalIP();
-//		logger.log("localip = " + localip + "\n");		
-//		
-//		AccessManager.searchServer(localip, new SearchCallBack() {			
-//			@Override
-//			public void onSearch(ServerInfo info) {
-//				if(info == null)
-//				{
-//					//end of search
-//					logger.log("end of search.\n");
-//				}
-//				else
-//				{
-//					logger.log("found server: "
-//							+ info.EndPoint.getAddress().getHostAddress()
-//							+ ", "
-//							+ info.ServiceName
-//							+ "\n");
-//				}
-//			}
-//		}); 
-//
+		server.start(conf);
+
+		// now we are looking for server.
+		String localip = NetworkUtils.getLocalIP();
+		logger.log("localip = " + localip + "\n");
+
+		AccessManager.searchServer(localip, new SearchCallBack() {
+			@Override
+			public void onSearch(ServerInfo info) {
+				if (info == null) {
+					// end of search
+					logger.log("end of search.\n");
+				} else {
+					logger.log("found server: "
+							+ info.EndPoint.getAddress().getHostAddress()
+							+ ", " + info.ServiceName + "\n");
+				}
+			}
+		});
+
 		logger.log("press enter to finish.\n");
 		waitfor();
-		
+
 		server.shutdown();
 	}
-	
-	public static void testServer(){
 
-		//server side
+	public static void testServer() {
+
+		// server side
 		server = new AccessManagerServer();
 		ServerConfig conf = new ServerConfig();
 		conf.Timeout = Constants.Default_Timeout;
 		conf.Port = ServerPort;
 		conf.CheckingPeriod = CheckingPeriod;
 		conf.serviceName = "TestService";
-		server.setConnectHandler(new ConnectHandler() {			
+		server.setConnectHandler(new ConnectHandler() {
 			@Override
 			public void onConnect(int connid) {
-				logger.log("(server): new connection is accepted - " + connid + "\n");				
+				logger.log("(server): new connection is accepted - " + connid
+						+ "\n");
 			}
 		});
 		server.setDisconnectHandler(new DisconnectHandler() {
-			
+
 			@Override
 			public void onDisconnect(int connid) {
-				logger.log("(server): client is disconnected - " + connid + "\n");				
+				logger.log("(server): client is disconnected - " + connid
+						+ "\n");
 			}
 		});
-		server.setReceiveHandler(new ServerReceiveHandler() {			
+		server.setReceiveHandler(new ServerReceiveHandler() {
 			@Override
 			public void onReceive(Packet pkt, int connid) {
 				logger.log("(server): a packet from client - " + connid + "\n");
@@ -106,90 +103,94 @@ public class Test {
 			}
 		});
 		server.start(conf);
-		
-		//end of test		
+
+		// end of test
 		ThreadEx.sleep(1000);
 		logger.log("press enter to terminate server.\n");
 		waitfor();
-		
-		//wait until server drop client.
+
+		// wait until server drop client.
 		logger.log("waiting until server drop client.\n");
 		ThreadEx.sleep(Constants.Default_Timeout + 1000);
 		server.shutdown();
-		
+
 		logger.log("finishing test...\n");
 	}
-	
-	public static void testClient(){
-		
-		//client side
+
+	public static void testClient() {
+
+		// client side
 		client = new AccessManager();
 		ServerInfo svinfo = new ServerInfo();
 		svinfo.EndPoint = new InetSocketAddress(ServerIP, ServerPort);
-		client.setReceiveHandler(new ReceiveHandler() {			
+		client.setReceiveHandler(new ReceiveHandler() {
 			@Override
 			public void onReceive(Packet pkt) {
-				logger.log("(client): a packet from server - " + (String)pkt.pop() + "\n");
+				logger.log("(client): a packet from server - "
+						+ (String) pkt.pop() + "\n");
 			}
 		});
 
 		client.connect(svinfo);
-		
+
 		Packet pkt = new Packet();
 		pkt.signiture = 1;
 		pkt.push("a string on packet");
-		
-		ThreadEx.sleep(100);
-		client.send(pkt);		
 
-		//end of test		
+		ThreadEx.sleep(100);
+		client.send(pkt);
+
+		// end of test
 		ThreadEx.sleep(5000);
 		client.dispose();
 	}
-	
-	public static void testAccessManager(){
-		
+
+	public static void testAccessManager() {
+
 		ThreadEx.invoke(null, new ActionEx() {
-			
+
 			@Override
 			public void work(Object arg) {
-				//will die after 5000 ms.
+				// will die after 5000 ms.
 				testClient();
 			}
 		});
-		
+
 		testServer();
-		
+
 	}
-	
-	public static void testTransceiver(){
-		ThreadEx.invoke(null, new ActionEx() {			
+
+	public static void testTransceiver() {
+		ThreadEx.invoke(null, new ActionEx() {
 			@Override
 			public void work(Object arg) {
 				logger.log("thread invoked\n");
-				Transceiver t = new Transceiver(new InetSocketAddress(ServerIP, 0), ServerPort);
+				Transceiver t = new Transceiver(new InetSocketAddress(ServerIP,
+						0), ServerPort);
 				Packet p = new Packet();
 				InetSocketAddress sender = t.receive(p);
-				logger.log(sender.getAddress().toString() + ":" + Integer.toString(sender.getPort()) + "\n");
+				logger.log(sender.getAddress().toString() + ":"
+						+ Integer.toString(sender.getPort()) + "\n");
 				logger.log(p.pop());
-				
+
 				t.dispose();
 			}
 		});
-		
+
 		ThreadEx.sleep(1000);
-		Transceiver t = new Transceiver(new InetSocketAddress(ServerIP, ServerPort));
+		Transceiver t = new Transceiver(new InetSocketAddress(ServerIP,
+				ServerPort));
 		Packet p = new Packet();
 		p.push("trasceiver test...\n");
 		boolean check = t.send(p);
-		logger.log(Boolean.toString(check)+"\n");
+		logger.log(Boolean.toString(check) + "\n");
 		logger.log("sent msg...\n");
-		
+
 		t.dispose();
-		
+
 		waitfor();
 	}
-	
+
 	public static void testSerializer() {
 		// TODO Auto-generated method stub
 		Packet pkt = new Packet();
@@ -202,7 +203,7 @@ public class Test {
 
 			pkt.signiture = -1;
 			pkt.push(100);
-			//pkt.push(200L);
+			// pkt.push(200L);
 			pkt.push(1.0f);
 			pkt.push(2.0);
 			pkt.push("hello");
@@ -225,10 +226,9 @@ public class Test {
 
 			logger.log(pkt.signiture);
 			logger.log("\n");
-			
+
 			while (pkt2.getElementCount() > 0) {
-				if (!pkt2.getTopElementType()
-						.equals(Packet.DataType_ByteArray))
+				if (!pkt2.getTopElementType().equals(Packet.DataType_ByteArray))
 					logger.log(pkt2.pop() + "\n");
 				else {
 					byte[] result = (byte[]) pkt2.pop();
