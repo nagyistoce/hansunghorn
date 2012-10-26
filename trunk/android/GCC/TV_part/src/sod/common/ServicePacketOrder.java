2ab2be72acc795a0ed6ef4f5a92abff73f2443d4
djawodud87 <djawodud87@b39bc24b-db09-0794-a29f-0fc425b6f3a2>
2012-10-26 10:26:15 +0000

    다운로드 모듈 안정화 및 최적화

---
+++ trunk/android/GCC/TV_part/src/sod/common/ServicePacketOrder.java
@@ -0,0 +1,37 @@
+package sod.common;
+
+public class ServicePacketOrder {
+
+	int order;
+	int flag_order;
+	
+	public ServicePacketOrder(){
+		order = 0;
+		flag_order = -1;
+	}
+	
+	public void initServicePacketOrder(){
+		order = 0;
+		flag_order = -1;
+	}
+	
+	public void increaseOrder(){
+		order++;
+		flag_order++;
+	}
+	
+	public int getOrder(){
+		return order;
+	}
+	
+	public boolean isCorrectOrder(int number){
+		
+		boolean returnData;
+		if(flag_order == (number-1))
+			returnData = true;
+		else
+			returnData = false;
+		
+		return returnData;
+	}
+}
